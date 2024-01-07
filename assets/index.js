fcd387b0890b6f32ca52b854dd6c4aaaecc0a066
votantaiH <153096700+votantaiH@users.noreply.github.com>
2024-01-07 20:42:33 +0700

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,204 @@
+const data = [
+    {
+        "imdbId": "tt3915174",
+        "title": "Puss in Boots: The Last Wish",
+        "releaseDate": "2022-12-21",
+        "trailerLink": "https://www.youtube.com/embed/tHb7WlgyaUc",
+        "genres": [
+            "Animation",
+            "Action",
+            "Adventure",
+            "Comedy",
+            "Family"
+        ],
+        "poster": "https://image.tmdb.org/t/p/w500/1NqwE6LP9IEdOZ57NCT51ftHtWT.jpg",
+        "backdrops": [
+            "https://image.tmdb.org/t/p/original/r9PkFnRUIthgBp2JZZzD380MWZy.jpg",
+            "https://image.tmdb.org/t/p/original/faXT8V80JRhnArTAeYXz0Eutpv9.jpg",
+            "https://image.tmdb.org/t/p/original/pdrlEaknhta2wvE2dcD8XDEbAI4.jpg",
+            "https://image.tmdb.org/t/p/original/tGwO4xcBjhXC0p5qlkw37TrH6S6.jpg",
+            "https://image.tmdb.org/t/p/original/cP8YNG3XUeBmO8Jk7Skzq3vwHy1.jpg",
+            "https://image.tmdb.org/t/p/original/qLE8yuieTDN93WNJRmFSAEJChOg.jpg",
+            "https://image.tmdb.org/t/p/original/vNuHqmOJRQXY0PBd887DklSDlBP.jpg",
+            "https://image.tmdb.org/t/p/original/uUCc62M0I3lpZy0SiydbBmUIpNi.jpg",
+            "https://image.tmdb.org/t/p/original/2wPJIFrBhzzAP8oHDOlShMkERH6.jpg",
+            "https://image.tmdb.org/t/p/original/fnfirCEDIkxZnQEtEMMSgllm0KZ.jpg"
+        ],
+        "reviewIds": []
+    },
+    {
+        "imdbId": "tt1630029",
+        "title": "Avatar: The Way of Water",
+        "releaseDate": "2022-12-16",
+        "trailerLink": "https://www.youtube.com/embed/d9MyW72ELq0",
+        "genres": [
+            "Science Fiction",
+            "Action",
+            "Adventure"
+        ],
+        "poster": "https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg",
+        "backdrops": [
+            "https://image.tmdb.org/t/p/original/s16H6tpK2utvwDtzZ8Qy4qm5Emw.jpg",
+            "https://image.tmdb.org/t/p/original/evaFLqtswezLosllRZtJNMiO1UR.jpg",
+            "https://image.tmdb.org/t/p/original/198vrF8k7mfQ4FjDJsBmdQcaiyq.jpg",
+            "https://image.tmdb.org/t/p/original/zaapQ1zjKe2BGhhowh5pM251Gpl.jpg",
+            "https://image.tmdb.org/t/p/original/tQ91wWQJ2WRNDXwxuO7GCXX5VPC.jpg",
+            "https://image.tmdb.org/t/p/original/5gPQKfFJnl8d1edbkOzKONo4mnr.jpg",
+            "https://image.tmdb.org/t/p/original/2fS9cpar9rzxixwnRptg4bGmIym.jpg",
+            "https://image.tmdb.org/t/p/original/fkGR1ltNbvERk3topo4dP3gWsvR.jpg",
+            "https://image.tmdb.org/t/p/original/rb9IHprKNoSKqatP2vr25unUDSu.jpg",
+            "https://image.tmdb.org/t/p/original/37ZswIuRQcRBN7kHij5MBjzRMRt.jpg"
+        ],
+        "reviewIds": []
+    },
+    {
+        "imdbId": "tt8760708",
+        "title": "M3GAN",
+        "releaseDate": "2023-01-06",
+        "trailerLink": "https://www.youtube.com/embed/BRb4U99OU80",
+        "genres": [
+            "Science Fiction",
+            "Horror",
+            "Comedy"
+        ],
+        "poster": "https://image.tmdb.org/t/p/w500/xBl5AGw7HXZcv1nNXPlzGgO4Cfo.jpg",
+        "backdrops": [
+            "https://image.tmdb.org/t/p/original/5kAGbi9MFAobQTVfK4kWPnIfnP0.jpg",
+            "https://image.tmdb.org/t/p/original/dlxzUj7z1MqEcFiwvvrj0bvBKDY.jpg",
+            "https://image.tmdb.org/t/p/original/q2fY4kMXKoGv4CQf310MCxpXlRI.jpg",
+            "https://image.tmdb.org/t/p/original/pTxwFdsdDWzpCRYuk1QbggdaOlL.jpg",
+            "https://image.tmdb.org/t/p/original/1zuz2RgFoOjulkjjNHHFc3WiHGB.jpg",
+            "https://image.tmdb.org/t/p/original/7HqxI1IXMloT9VTSuDC8ikaj810.jpg",
+            "https://image.tmdb.org/t/p/original/vpK2rp3J5LiC01HoNM0j9DEHQ1T.jpg",
+            "https://image.tmdb.org/t/p/original/cNHXdmr4amP6EPCMa0dqD8rwzDV.jpg",
+            "https://image.tmdb.org/t/p/original/txQLFd6rfQrskQhFENkS1jElptt.jpg",
+            "https://image.tmdb.org/t/p/original/dC4tj1ONdlZ2TWv4XD2SA1KUnJN.jpg"
+        ],
+        "reviewIds": []
+    },
+    {
+        "imdbId": "tt11116912",
+        "title": "Troll",
+        "releaseDate": "2022-12-01",
+        "trailerLink": "https://www.youtube.com/embed/AiohkY_XQYQ",
+        "genres": [
+            "Fantasy",
+            "Action",
+            "Adventure"
+        ],
+        "poster": "https://image.tmdb.org/t/p/w500/9z4jRr43JdtU66P0iy8h18OyLql.jpg",
+        "backdrops": [
+            "https://image.tmdb.org/t/p/original/53BC9F2tpZnsGno2cLhzvGprDYS.jpg",
+            "https://image.tmdb.org/t/p/original/e9Qb2kmBnMXHCmNMI8NX1JbWhh1.jpg",
+            "https://image.tmdb.org/t/p/original/2WjOOOGUu6dp4r8VqR5n48DY7JG.jpg",
+            "https://image.tmdb.org/t/p/original/duIsyybgrC4S8kcCIVaxNOttV15.jpg",
+            "https://image.tmdb.org/t/p/original/3RS8runn9AfrYDzRVPWuGPmvXQf.jpg",
+            "https://image.tmdb.org/t/p/original/8wLRn2VvBlCu6cqYS4ypipnwosr.jpg",
+            "https://image.tmdb.org/t/p/original/zDqVVkmfvj47FBUE5lwE4rWnITu.jpg",
+            "https://image.tmdb.org/t/p/original/682Ui5DwZDdbIPzKAEOR7cJlMXa.jpg",
+            "https://image.tmdb.org/t/p/original/6jdlppcnGi3XuJamfs4Vl7HyxB.jpg",
+            "https://image.tmdb.org/t/p/original/uIq83ogs7QBEWi1aqmUrdDIH61m.jpg"
+        ],
+        "reviewIds": []
+    },
+    {
+        "imdbId": "tt6443346",
+        "title": "Black Adam",
+        "releaseDate": "2022-10-19",
+        "trailerLink": "https://www.youtube.com/embed/JaV7mmc9HGw",
+        "genres": [
+            "Fantasy",
+            "Action",
+            "Science Fiction"
+        ],
+        "poster": "https://image.tmdb.org/t/p/w500/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
+        "backdrops": [
+            "https://image.tmdb.org/t/p/original/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
+            "https://image.tmdb.org/t/p/original/9hNtTwY8P5v2MKnUeb7iuREI7Yb.jpg",
+            "https://image.tmdb.org/t/p/original/zplntIhzXyBiXFYWReETxh0uyFF.jpg",
+            "https://image.tmdb.org/t/p/original/yxkhM18dYwsRRffLnd9lz2d4i0v.jpg",
+            "https://image.tmdb.org/t/p/original/bgaBKREAfUtZgvd6zoV6RQRcIUt.jpg",
+            "https://image.tmdb.org/t/p/original/uqYxoj4hqwocwfBs2xxGyQT88Yk.jpg",
+            "https://image.tmdb.org/t/p/original/qBx97wytqlyPqXATHqRgIVFxJRU.jpg",
+            "https://image.tmdb.org/t/p/original/d6MhreFdMHONqX3iZlJGCF8UkIt.jpg",
+            "https://image.tmdb.org/t/p/original/9inNotReApz0n50WvWbrt0n1cbL.jpg",
+            "https://image.tmdb.org/t/p/original/pSOuqtJmdh7aI1yiK7M8e0PmbPC.jpg"
+        ],
+        "reviewIds": []
+    },
+    {
+        "imdbId": "tt0499549",
+        "title": "Avatar",
+        "releaseDate": "2009-12-15",
+        "trailerLink": "https://www.youtube.com/embed/5PSNL1qE6VY",
+        "genres": [
+            "Fantasy",
+            "Action",
+            "Science Fiction",
+            "Adventure"
+        ],
+        "poster": "https://image.tmdb.org/t/p/w500/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg",
+        "backdrops": [
+            "https://image.tmdb.org/t/p/original/o0s4XsEDfDlvit5pDRKjzXR4pp2.jpg",
+            "https://image.tmdb.org/t/p/original/8I37NtDffNV7AZlDa7uDvvqhovU.jpg",
+            "https://image.tmdb.org/t/p/original/2YLOjUiczXEgVZFDSIeH3iWB3Ub.jpg",
+            "https://image.tmdb.org/t/p/original/Yc9q6QuWrMp9nuDm5R8ExNqbEq.jpg",
+            "https://image.tmdb.org/t/p/original/jlQJDD0L5ZojjlS0KYnApdO0n19.jpg",
+            "https://image.tmdb.org/t/p/original/sfw4m2tOgQRzhF6VXxaXGfd1vX.jpg",
+            "https://image.tmdb.org/t/p/original/7ABsaBkO1jA2psC8Hy4IDhkID4h.jpg",
+            "https://image.tmdb.org/t/p/original/xMMrBziwJqrgjerqpNeQvwuwiUp.jpg",
+            "https://image.tmdb.org/t/p/original/chauy3iJaZtrMbTr72rgNmOZwo3.jpg",
+            "https://image.tmdb.org/t/p/original/mYJkJ7YxJsUNI1nAOOUOpRN2auC.jpg"
+        ],
+        "reviewIds": []
+    },
+
+]
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const show = $('.trailer');
+const trailer = $('iframe');
+const closeBtn = $('.trailer .close');
+
+function render(slection) {
+    const container = $(slection);
+    let text = data.map(movie => {
+        return `<div class="movie" data=${movie.trailerLink}>
+                    <div class="movie-image"> <span class="play"><span class="name">X-MAN</span></span>
+                     <img src="${movie.poster}" alt="" /> 
+                     </div>
+                        <div class="rating">
+                            <div class='stars'>
+                            <div class='stars-in'>
+                            </div>
+                            </div>
+                        </div>
+                        <span class="comments">${movie.reviewIds.length || Math.floor(Math.random() * 1000)}</span>
+                </div>`;
+    }).join('');
+    container.innerHTML = text;
+}
+
+function stopVideo() {
+    show.style.display = 'none';
+    trailer.src = null;
+}
+
+render('#lastest-trailers');
+render('#top-rated');
+render('#most-commented');
+
+const movies = $$('.movie');
+
+
+for (let i = 0; i < movies.length; i++) {
+    movies[i].onclick = () => {
+        show.style.display = 'flex';
+        trailer.src = movies[i].getAttribute('data');
+    }
+}
+
+closeBtn.onclick = stopVideo;
+show.onclick = stopVideo;
